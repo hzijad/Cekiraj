aba242a62607d27e542882c675f0be698bd1acb2
ZIKE <36049864+hzijad@users.noreply.github.com>
2024-10-02 18:26:43 +0200

    Create list.js
    Added manual list of sites

---
+++ list.js
@@ -0,0 +1,2 @@
+//before contacting raskrinkavanje or others IFCN sites using this manual list
+const targetDomains = ['https://povjerljivo.com/', 'https://nultatacka.rs/', 'https://vecernjenovosti.ba/'];
