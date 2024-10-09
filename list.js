fbd9756a3e143ceac4611c11b8a4cb5c497a18d6
ZIKE <36049864+hzijad@users.noreply.github.com>
2024-10-09 14:23:26 +0200

    removed contentscript code

--- list.js
+++ list.js
@@ -1,2 +1,2 @@
 //before contacting raskrinkavanje or others IFCN sites using this manual list
-const targetDomains = ['https://povjerljivo.com/', 'https://nultatacka.rs/', 'https://vecernjenovosti.ba/'];
+const targetDomains = ['povjerljivo.com', 'nultatacka.rs', 'vecernjenovosti.ba'];
