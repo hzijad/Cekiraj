ebf7cb3a25c0f3132c71d09d35bafee5936b3da0
ZIKE <36049864+hzijad@users.noreply.github.com>
2024-10-25 21:07:37 +0200

    Newest code

--- background.js
+++ background.js
@@ -39,9 +39,9 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 });
 
 function determineTrustLevel(articles) {
-  if (articles >= 200) {
+  if (articles <= 100) {
     return "Povjerena";
-  } else if (articles >= 100) {
+  } else if (articles <= 200 && articles >100) {
     return "Povjerenija od ostalih";
   } else {
     return "Manje povjerena";
@@ -59,154 +59,154 @@ function displayTrustLevelPopup(trustLevel) {
 
   if (trustLevel === "Povjerena") {
     checkmarkIcon = chrome.runtime.getURL('images/green-checkmark.png'); // Correct path to the image inside the 'images' folder
-    trustColor = 'green';
+    trustColor = '#276D30'; // Green color
   } else if (trustLevel === "Povjerenija od ostalih") {
     checkmarkIcon = chrome.runtime.getURL('images/yellow-checkmark.png'); // Correct path to the yellow checkmark
-    trustColor = 'yellow';
+    trustColor = '#B7AB17'; // Yellow color
   } else {
     checkmarkIcon = chrome.runtime.getURL('images/red-checkmark.png'); // Correct path to the red checkmark
-    trustColor = 'red';
+    trustColor = '#B71335'; // Red color
   }
 
   const logoUrl = chrome.runtime.getURL('images/logo.png'); // Path to the logo image
-  const warningSignUrl = chrome.runtime.getURL('images/warning-sign.png'); // Path to the warning sign image
 
   shadowRoot.innerHTML = `
-    <style>
-      body, html {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-      }
+  <style>
+    body, html {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
 
-      .modal-overlay {
-        position: fixed;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        left: 0;
-        z-index: 9999;
-        opacity: 0;
-        visibility: hidden;
-        background: rgba(0, 0, 0, 0.5);
-        transition: opacity 0.3s ease, visibility 0.3s ease;
-      }
+    .modal-overlay {
+      position: fixed;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      z-index: 9999;
+      opacity: 0;
+      visibility: hidden;
+      background: rgba(0, 0, 0, 0.5);
+      transition: opacity 0.3s ease, visibility 0.3s ease;
+    }
 
-      .modal-overlay.active {
-        opacity: 1;
-        visibility: visible;
-      }
+    .modal-overlay.active {
+      opacity: 1;
+      visibility: visible;
+    }
 
+    .modal {
+      background-color: white;
+      border-radius: 8px;
+      padding: 1rem;
+      box-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
+      max-width: 300px;
+      width: 100%;
+      position: absolute;
+      top: 20px !important;
+      right: 20px !important;
+      z-index: 10000 !important;
+      text-align: center;
+      overflow: hidden;
+    }
+
+    .close-modal {
+      position: absolute;
+      top: 10px;
+      right: 10px;
+      cursor: pointer;
+    }
+
+    .close-modal svg {
+      width: 20px;
+      height: 20px;
+    }
+
+    .modal-content {
+      font-size: 16px;
+      color: #333;
+      margin: 20px 0;
+      text-align: center; /* Center the text */
+    }
+
+    .warning-text {
+      font-size: 14px; /* Make the text a bit smaller */
+      color: #333;
+      font-weight: bold;
+      margin: 10px 0;
+    }
+
+    .trust-level {
+      font-size: 14px;
+      color: ${trustColor};
+      font-weight: bold;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-top: 20px; /* Add space between warning text and trust level */
+    }
+
+    .trust-level img {
+      width: 20px;
+      height: 20px;
+      margin-right: 5px;
+    }
+
+    .logo {
+      width: 80px; /* Slightly reduce size */
+      height: auto;
+      margin: 0 auto;
+      display: block;
+    }
+    
+    .warning-title {
+      font-size: 1.6em;
+      font-weight: bold;
+      text-align: center;
+      margin-bottom: 1em;
+    }
+
+    .separator {
+      width: 100%;
+      height: 1px;
+      background-color: #ccc;
+      margin: 15px 0; /* Slightly increase gap between logo and line */
+    }
+
+    @media only screen and (max-width: 600px) {
       .modal {
-        background-color: white;
-        border-radius: 8px;
-        padding: 1rem;
-        box-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
-        max-width: 300px;
-        width: 100%;
-        position: absolute;
-        top: 20px !important;
-        right: 20px !important;
-        z-index: 10000 !important;
-        text-align: center;
-        overflow: hidden;
-      }
-
-      .close-modal {
-        position: absolute;
-        top: 10px;
-        right: 10px;
-        cursor: pointer;
-      }
-
-      .close-modal svg {
-        width: 20px;
-        height: 20px;
+        width: 90%;
+        max-width: 90%;
       }
 
       .modal-content {
-        font-size: 16px;
-        color: #333;
-        margin: 20px 0;
-        text-align: center; /* Center the text */
-      }
-
-      .warning-icon {
-        width: 20px;
-        height: 20px;
-        margin-bottom: 5px; /* Add some space below the icon */
-      }
-
-      .warning-text {
-        font-size: 14px; /* Make the text a bit smaller */
-        color: #333;
-        font-weight: bold;
-        margin: 10px 0;
-      }
-
-      .trust-level {
         font-size: 14px;
-        color: ${trustColor};
-        font-weight: bold;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        margin-top: 20px; /* Add space between warning text and trust level */
       }
+    }
+  </style>
 
-      .trust-level img {
-        width: 20px;
-        height: 20px;
-        margin-right: 5px;
-      }
-
-      .logo {
-        width: 100px;
-        height: auto;
-        margin: 0 auto;
-        display: block;
-      }
-
-      .separator {
-        width: 100%;
-        height: 1px;
-        background-color: #ccc;
-        margin: 20px 0;
-      }
-
-      @media only screen and (max-width: 600px) {
-        .modal {
-          width: 90%;
-          max-width: 90%;
-        }
-
-        .modal-content {
-          font-size: 14px;
-        }
-      }
-    </style>
-
-    <div class="modal-overlay">
-      <div class="modal">
-        <a class="close-modal" aria-label="Close modal">
-          <svg viewBox="0 0 20 20">
-            <path d="M15.898,4.045c-0.271-0.272-0.713-0.272-0.986,0l-4.71,4.711L5.493,4.045c-0.272-0.272-0.714-0.272-0.986,0s-0.272,0.714,0,0.986l4.709,4.711l-4.71,4.711c-0.272,0.271-0.272,0.713,0,0.986c0.136,0.136,0.314,0.203,0.492,0.203c0.179,0,0.357-0.067,0.493-0.203l4.711-4.711l4.71,4.711c0.137,0.136,0.314,0.203,0.494,0.203c0.178,0,0.355-0.067,0.492-0.203c0.273-0.273,0.273-0.715,0-0.986l-4.711-4.711l4.711-4.711C16.172,4.759,16.172,4.317,15.898,4.045z"></path>
-          </svg>
-        </a>
-        <img src="${logoUrl}" alt="Logo" class="logo">
-        <div class="separator"></div>
-        <div class="modal-content">
-          <img src="${warningSignUrl}" alt="Warning Sign" class="warning-icon">
-          <div class="warning-text">
-            Ova stranica je zabilježena da objavljuje neistinite sadržaje, upozorenje preporučeno prilikom čitanja.
-          </div>
-          <div class="trust-level">
-            <img src="${checkmarkIcon}" alt="Trust Level Icon">
-            ${trustLevel}
-          </div>
+  <div class="modal-overlay">
+    <div class="modal">
+      <a class="close-modal" aria-label="Close modal">
+        <svg viewBox="0 0 20 20">
+          <path d="M15.898,4.045c-0.271-0.272-0.713-0.272-0.986,0l-4.71,4.711L5.493,4.045c-0.272-0.272-0.714-0.272-0.986,0s-0.272,0.714,0,0.986l4.709,4.711l-4.71,4.711c-0.272,0.271-0.272,0.713,0,0.986c0.136,0.136,0.314,0.203,0.492,0.203c0.179,0,0.357-0.067,0.493-0.203l4.711-4.711l4.71,4.711c0.137,0.136,0.314,0.203,0.494,0.203c0.178,0,0.355-0.067,0.492-0.203c0.273-0.273,0.273-0.715,0-0.986l-4.711-4.711l4.711-4.711C16.172,4.759,16.172,4.317,15.898,4.045z"></path>
+        </svg>
+      </a>
+      <img src="${logoUrl}" alt="Logo" class="logo">
+      <div class="separator"></div>
+      <div class="modal-content">
+        <div class="warning-title">Upozorenje</div>
+        <div class="warning-text">
+          Ova stranica je zabilježena da objavljuje neistinite sadržaje, upozorenje preporučeno prilikom čitanja.
+        </div>
+        <div class="trust-level">
+          <img src="${checkmarkIcon}" alt="Trust Level Icon">
+          ${trustLevel}
         </div>
       </div>
     </div>
+  </div>
   `;
 
   // Modal functionality
